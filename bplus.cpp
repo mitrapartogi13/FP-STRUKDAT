bc9543e3a36f68cc959b2ffad8bf35051ab248c8
mitrapartogi13 <mitrapartogi@gmail.com>
2025-06-26 14:31:41 +0700

    ayooooo

---
+++ bplus.cpp
@@ -0,0 +1,30 @@
+// filepath: /home/mitrap13/kuliah/StrukturData/StrukturData/FP-STRUKDAT/src/bplustree.cpp
+
+#include "bplustree.h"
+#include <iostream>
+#include <vector>
+
+BPlusTree::BPlusTree(int order) : order(order), root(nullptr) {}
+
+void BPlusTree::insert(int key) {
+    // Implementation of insert method
+}
+
+void BPlusTree::remove(int key) {
+    // Implementation of remove method
+}
+
+bool BPlusTree::search(int key) {
+    // Implementation of search method
+    return false; // Placeholder return
+}
+
+void BPlusTree::print() {
+    // Implementation of print method for debugging
+}
+
+void BPlusTree::clear() {
+    // Implementation of clear method to delete all nodes
+}
+
+// Additional private methods for B+ Tree operations can be added here.
---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+
+signed main(){
+    ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+
+    while(1){
+        cout << "Selamat datang di Program Perbandingan B+ tree dan Hash table\n";
+        cout << "Pilihan : " << endl;
+        cout << "1. B+ tree" << endl;
+        cout << "2. Hash table" << endl;
+        cout << "3. Keluar" << endl;
+        int pilihan; cin >> pilihan;
+        if (pilihan == 1) {
+            cout << "Anda memilih B+ tree" << endl;
+
+            
+        } else if (pilihan == 2) {
+            cout << "Anda memilih Hash table" << endl;
+            
+        } else if (pilihan == 3) {
+            cout << "Terima kasih telah menggunakan program ini!" << endl;
+            break; 
+        } else {
+            cout << "Pilihan tidak valid, silakan coba lagi." << endl;
+        }
+        
+        
+        
+        _
+    }
+
